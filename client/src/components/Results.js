81899d7be1751e5efc36742ed2638eec71a52555
Nathan Bernabe <nbernabe09@aol.com>
2017-11-08 10:39:29 -0800

    Stuck on sending state.results

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -4,7 +4,7 @@ class Results extends Component {
   render() {
     return (
       <div className="card-body">
-        
+        <p>This area will display updated state.results</p>
       </div>
     );
   }
